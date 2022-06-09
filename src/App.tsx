5c7d8f55352a792ad427af32837c9a626dedb398
Nikita Parfenov <nktpfn@yandex-team.ru>
2022-06-09 17:30:53 +0300

    Add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import { AddItemForm } from './containers/AddItemForm';
+import { ItemsList } from './containers/ItemsList';
+import { ItemsProvider } from './contexts/Items';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ItemsProvider>
+      <Header />
+      <AddItemForm />
+      <ItemsList />
+    </ItemsProvider>
   );
-}
-
-export default App;
+};
